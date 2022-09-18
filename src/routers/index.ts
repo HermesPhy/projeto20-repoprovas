44f62de1a011583193da78246e146b4f6e4fb3a6
Hermes Júnior <hermesj.junior@gmail.com>
2022-09-17 22:17:07 -0300

    userRouter settings

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
+import userRouter from "./userRouter";
 //import categoryRouter from "./categoryRouter";
 //import disciplineRouter from "./disciplineRouter";
 //import teacherRouter from "./teacherRouter";
 //import testRouter from "./testRouter";
-//import userRouter from "./userRouter";
 
 const router = Router();
 
-//router.use(userRouter);
+router.use(userRouter);
 //router.use(testRouter);
 //router.use(categoryRouter);
 //router.use(disciplineRouter);
---
+++ src/routers/userRouter.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+//import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware";
+//import { userSchema } from "../schemas/userSchema";
+//import userController from "../controllers/userController";
+
+const userRouter = Router();
+
+//userRouter.post("/sign-up", validateSchemaMiddleware(userSchema), userController.signUp);
+//userRouter.post("/sign-in", validateSchemaMiddleware(userSchema), userController.signIn);
+
+export default userRouter;
