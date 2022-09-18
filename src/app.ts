4f8d627f2f205dc19a863d1b2b52761677cc5723
Hermes Júnior <hermesj.junior@gmail.com>
2022-09-17 22:04:02 -0300

    Routers settings

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import cors from "cors";
 import express, { json } from "express";
 import "express-async-errors";
 
-//import router from "./routers/index";
+import router from "./routers/index";
 //import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 
 const app = express();
 app.use(json());
 app.use(cors());
-//app.use(router);
+app.use(router);
 //app.use(errorHandlerMiddleware);
 
 export default app;
