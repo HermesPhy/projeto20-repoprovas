ef81595841daa971c33a0feabd0b685fa0e75430
Hermes Júnior <hermesj.junior@gmail.com>
2022-09-19 11:57:45 -0300

    Seed settings

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,12 @@
 import { prisma } from "../src/database";
 
 async function main() {
-  await prisma.categories.createMany({
+  await prisma.category.createMany({
     data: [{ name: "Projeto" }, { name: "Prática" }, { name: "Recuperação" }],
     skipDuplicates: true,
   });
 
-  await prisma.teachers.createMany({
+  await prisma.teacher.createMany({
     data: [
       { name: "Diego M. Pinho" },
       { name: "Bruna Hamori" },
@@ -15,12 +15,12 @@ async function main() {
     skipDuplicates: true,
   });
 
-  await prisma.terms.createMany({
+  await prisma.term.createMany({
     data: [{ number: 1 }, { number: 2 }, { number: 3 }],
     skipDuplicates: true,
   });
 
-  await prisma.disciplines.createMany({
+  await prisma.discipline.createMany({
     data: [
       { name: "HTML e CSS", termId: 1 },
       { name: "JavaScript", termId: 2 },
@@ -34,7 +34,7 @@ async function main() {
     skipDuplicates: true,
   });
 
-  await prisma.teachersDisciplines.createMany({
+  await prisma.teacherDiscipline.createMany({
     data: [
       { disciplineId: 1, teacherId: 1 },
       { disciplineId: 2, teacherId: 1 },
@@ -52,67 +52,67 @@ async function main() {
     skipDuplicates: true,
   });
 
-  await prisma.tests.createMany({
+  await prisma.test.createMany({
     data: [
       {
         name: "Projeto 1 - Globo.com",
         pdfUrl: "https://globo.com",
-        categoriesId: 1,
-        teachersDisciplinesId: 1,
+        categoryId: 1,
+        teacherDisciplineId: 1,
       },
       {
         name: "Projeto 3 - Parrot Game",
         pdfUrl: "https://google.com?q=parrots",
-        categoriesId: 1,
-        teachersDisciplinesId: 2,
+        categoryId: 1,
+        teacherDisciplineId: 2,
       },
       {
         name: "Projeto 5 - Instragram React",
         pdfUrl: "https://instagram.com",
-        categoriesId: 1,
-        teachersDisciplinesId: 3,
+        categoryId: 1,
+        teacherDisciplineId: 3,
       },
       {
         name: "Recuperação de HTML e CSS",
         pdfUrl: "https://google.com?q=recuperacao",
-        categoriesId: 3,
-        teachersDisciplinesId: 1,
+        categoryId: 3,
+        teacherDisciplineId: 1,
       },
       {
         name: "Prática - MBTI",
         pdfUrl: "https://google.com?q=mbti",
-        categoriesId: 2,
-        teachersDisciplinesId: 4,
+        categoryId: 2,
+        teacherDisciplineId: 4,
       },
       {
         name: "Prática - BatePapo",
         pdfUrl: "https://google.com?bate-papo",
-        categoriesId: 2,
-        teachersDisciplinesId: 4,
+        categoryId: 2,
+        teacherDisciplineId: 4,
       },
       {
         name: "Prática - Conversas difíceis",
         pdfUrl: "https://google.com?q=conversas",
-        categoriesId: 2,
-        teachersDisciplinesId: 5,
+        categoryId: 2,
+        teacherDisciplineId: 5,
       },
       {
         name: "Prática - Conversas difíceis",
         pdfUrl: "https://google.com?q=conversas",
-        categoriesId: 2,
-        teachersDisciplinesId: 6,
+        categoryId: 2,
+        teacherDisciplineId: 6,
       },
       {
         name: "Prática - HUB",
         pdfUrl: "https://google.com?q=conversas",
-        categoriesId: 2,
-        teachersDisciplinesId: 7,
+        categoryId: 2,
+        teacherDisciplineId: 7,
       },
       {
         name: "Prática - Recuperação",
         pdfUrl: "https://google.com?q=conversas",
-        categoriesId: 3,
-        teachersDisciplinesId: 8,
+        categoryId: 3,
+        teacherDisciplineId: 8,
       },
     ],
     skipDuplicates: true,
