7025c6363841b7a7b925368b2be049cb03a0405b
Hermes Júnior <hermesj.junior@gmail.com>
2022-09-19 12:25:09 -0300

    Disciplines settings

---
+++ src/routers/disciplineRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import disciplineController from "../controllers/disciplineController";
+import { ensureAuthenticatedMiddleware } from "../middlewares/ensureAuthenticatedMiddleware";
+
+const disciplineRouter = Router();
+
+disciplineRouter.get(
+  "/disciplines/:term",
+  ensureAuthenticatedMiddleware,
+  disciplineController.get
+);
+
+export default disciplineRouter;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,16 +2,16 @@ import { Router } from "express";
 
 import userRouter from "./userRouter";
 import testRouter from "./testRouter";
+import disciplineRouter from "./disciplineRouter";
 //import categoryRouter from "./categoryRouter";
-//import disciplineRouter from "./disciplineRouter";
 //import teacherRouter from "./teacherRouter";
 
 const router = Router();
 
 router.use(userRouter);
 router.use(testRouter);
+router.use(disciplineRouter);
 //router.use(categoryRouter);
-//router.use(disciplineRouter);
 //router.use(teacherRouter);
 
 export default router;
