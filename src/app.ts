a1a1b6d108c7455a35e7ba9726dd4d38780e6272
Hermes Júnior <hermesj.junior@gmail.com>
2022-09-19 15:34:02 -0300

    ErrorHandler settings

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,12 @@ import express, { json } from "express";
 import "express-async-errors";
 
 import router from "./routers/index";
-//import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 
 const app = express();
 app.use(json());
 app.use(cors());
 app.use(router);
-//app.use(errorHandlerMiddleware);
+app.use(errorHandlerMiddleware);
 
 export default app;
